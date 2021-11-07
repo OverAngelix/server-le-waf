337196edc955f2363f5602783aa8a2f46a6931d8
Steven Copin <steven.copin@univ-lille.fr>
2021-11-07 22:08:31 +0100

    FIX : protection BDD HOST

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "ec2-54-73-152-36.eu-west-1.compute.amazonaws.com",
+    HOST: process.env.HOST,
     USER: process.env.USER,
     PASSWORD: process.env.PASSWORD,
     DB: process.env.DB,
