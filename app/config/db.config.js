24bb8de48e3b337e2aff7fcaf5d36fc9510a769d
Steven Copin <steven.copin@univ-lille.fr>
2021-11-29 15:17:51 +0100

    CORRECTION : disparition des preuves

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  HOST:"ec2-54-73-152-36.eu-west-1.compute.amazonaws.com",
-  USER:"ewhqaiwlylrjsi",
-  PASSWORD:"9904bfb3f0665bcd4ad4d02ccf4e1a37f692748d7c1e85874ad20000093e89f8",
-  DB:"d3rng9a1j2ck1q",
-  dialect: "postgres",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
   native: true,
   ssl: true,
   pool: {
